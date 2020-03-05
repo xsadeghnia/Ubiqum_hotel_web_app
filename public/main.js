5b90affc7991b8400044e174edc75ad75ff0f68e
Afsaneh Sadeghnia <x.sadeghnia@gmail.com>
2020-03-05 22:17:49 +0100

    complete the project.

--- public/main.js
+++ public/main.js
@@ -8,15 +8,26 @@ const app = new Vue({
         allHotelsInfo: [],
         searchMode: false,
         moreInfoMode: false,
-        favouritesMode : false,
-        chatMode : false,
+        favouritesMode: false,
+        chatMode: false,
+        currentPage: 0,
+        previousPage: 0,
         selectedHotel: null,
         hotelImages: [],
         facilities: [],
         favArr: [],
+        posts: [],
+        text: "",
+        isLoggedIn: false,
+        username: "",
+        loading: false,
+        favIsSelected: false,
     },
     methods: {
         search: async function () {
+            this.loading = true;
+            this.previousPage = this.currentPage;
+            this.currentPage = 1;
             const targetURL = 'https://hotels4.p.rapidapi.com/locations/search?query=' + this.destination;
             const options = {
                 headers: new Headers({
@@ -60,8 +71,11 @@ const app = new Vue({
                 newFacilities.push(facil[11].label);
             this.facilities = newFacilities;
             this.searchMode = true;
+            this.loading = false;
         },
         showMoreInfo: async function (hotel) {
+            this.previousPage = this.currentPage;
+            this.currentPage = 2;
             this.selectedHotel = hotel;
             const options = {
                 headers: new Headers({
@@ -81,41 +95,150 @@ const app = new Vue({
         },
         markFavourites: async function (hotel) {
             let i = this.favArr.indexOf(hotel);
-            if(i != -1){
-                this.favArr.splice(i,1);
+            if (i != -1) {
+                this.favArr.splice(i, 1);
+                this.favIsSelected = false;
                 return;
             }
             this.favArr.push(hotel);
+            this.favIsSelected = true;
+
         },
-        showFavourites : async function(){
+        showFavourites: async function () {
+            // this.previousPage = this.currentPage;
+            // this.currentPage = 3;
             console.log("fav is clicked!");
-            this.moreInfoMode =false;
-            this.searchMode =false;
+            this.moreInfoMode = false;
+            this.searchMode = false;
             this.favouritesMode = true;
         },
+        closeFav: async function () {
+            console.log("current page:" + this.currentPage);
+            console.log("previous page:" + this.previousPage);
+            if (this.previousPage == 0) {
+                this.favouritesMode = false;
+            } else if (this.previousPage == 1) {
+                this.favouritesMode = false;
+                this.searchMode = true;
+            } else if (this.previousPage == 2) {
+                this.favouritesMode = false;
+                this.moreInfoMode = true;
+            }
+        },
         goBack: async function () {
-            this.moreInfoMode = false;
+            if (this.currentPage == 0) {
+                this.favouritesMode = false;
+            } else if (this.currentPage == 1) {
+                this.favouritesMode = false;
+                this.searchMode = true;
+            } else if (this.currentPage == 2) {
+                this.moreInfoMode = false;
+                this.searchMode = true;
+            }
+            var temp = this.previousPage;
+            this.previousPage = this.currentPage;
+            this.currentPage = temp;
+            console.log("back button is clicked!");
+            console.log("current page:" + this.currentPage);
+            console.log("previous page:" + this.previousPage);
         },
-        favBack: async function(){
-            this.favouritesMode = false;
-            this.searchMode = true;
-        },
-        goHome : async function(){
+        goHome: async function () {
             this.searchMode = false;
             this.moreInfoMode = false;
             this.favouritesMode = false;
             this.chatMode = false;
         },
-        chat : async function(){
+        chat: async function () {
+            // this.previousPage = this.currentPage;
+            // this.currentPage = 4;
             console.log("chat is clicked!");
             this.searchMode = false;
             this.moreInfoMode = false;
             this.favouritesMode = false;
             this.chatMode = true;
-            getPosts();
+            this.getPosts();
         },
-        closeChat: async function(){
-            this.chatMode = false;
+        closeChat: async function () {
+            if (this.previousPage == 0) {
+                this.chatMode = false;
+            } else if (this.previousPage == 1) {
+                this.chatMode = false;
+                this.searchMode = true;
+            } else if (this.previousPage == 2) {
+                this.chatMode = false;
+                this.moreInfoMode = true;
+            }
+        },
+        checkAuthState() {
+            firebase.auth().onAuthStateChanged(function (user) {
+                console.log(user)
+                if (user) {
+                    // User is signed in.
+                    app.isLoggedIn = true
+                    console.log('logged in')
+
+                } else {
+                    app.isLoggedIn = false
+                    console.log('logged out')
+                    // No user is signed in.
+                }
+            });
+        },
+        login() {
+            var provider = new firebase.auth.GoogleAuthProvider();
+
+            firebase.auth().signInWithPopup(provider)
+                .then(function () {
+                    app.getPosts();
+                })
+                .catch(function () {
+                    alert("Something went wrong");
+                });
+        },
+        logout() {
+            firebase.auth().signOut()
+                .then(function () {
+                    // Sign-out successful.
+                    console.log("user logged out.");
+                })
+                .catch(function (error) {
+                    // An error happened
+                    console.log("Something went wrong");
+                });
+        },
+        writeNewPost() {
+            var userName = firebase.auth().currentUser.displayName;
+            // A post entry.
+            var postData = {
+                name: userName,
+                body: this.text
+            };
+            // Get a key for a new Post.
+            var newPostKey = firebase.database().ref().child('myMatch').push().key;
+            var updates = {};
+            updates[newPostKey] = postData;
+            this.text = "";
+            // audio.play();
+            return firebase.database().ref().child('myMatch').update(updates);
+        },
+        getPosts() {
+            this.username = firebase.auth().currentUser.displayName
+            firebase.database().ref('myMatch').on('value', function (data) {
+                app.posts = data.val();
+                setTimeout(() => {
+                    var container = document.querySelector(".box");
+                    var scrollHeight = container.scrollHeight;
+                    container.scrollTop = scrollHeight;
+                }, 100)
+                // var messageDisplay = myVue.$refs.messageDisplay;
+                // messageDisplay.scrollTop = messageDisplay.scrollHeight;
+            })
+        },
+        addressConvert(string) {
+            return string.split(" ").join("+")
         }
     },
+    mounted() {
+        this.checkAuthState()
+    }
 });
